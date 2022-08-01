f1b31ff583669cddba2f5917f814ab2daf52f85c
valerybuko <valpal314@gmail.com>
2022-08-01 17:03:24 +0200

    [task-5] updated import api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://hy5qpxv52l.execute-api.eu-west-1.amazonaws.com/dev/',
+  import: 'https://b7l2dikl29.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
 };
